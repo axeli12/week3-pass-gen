3fa744f1b8dc01e1b4a042a7a36f501127c3ec92
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-01-17 21:09:21 -0700

    added main function

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener("click", function () {
+    pass = generatePassword();
+    document.getElementById("password").placeholder = pass;
+});
+
+// Start function to generate password
+function generatePassword() {
+    enter = prompt("How many characters would you like for password? Choose between 8-128");
+    if (!enter) {
+        alert("Please enter a number");
+    } else if (enter < 8) {
+        enter = (alert("Please choose a number higher than 7"));        
+    } else if (enter > 128) {
+        enter = (alert("Please choose a number less than 129"));
+    } else {
+        
+
+             }
+            }
+        
+
